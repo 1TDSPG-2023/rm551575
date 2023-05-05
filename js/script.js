6d9eb595f5f983521c7255552c158e09e1885c07
andresant-ana <rm551575@fiap.com.br>
2023-05-05 11:58:11 -0300

    Concatenação de arrays com spread.

--- js/script.js
+++ js/script.js
@@ -26,47 +26,60 @@
 // });
 
 
-/* ARRAYS E SEUS MÉTODOS */
+// /* ARRAYS E SEUS MÉTODOS */
 
-let frutas = ["Banana", "Maçã", "Uva", "Pêra", "Melancia", "Morango", "Goiaba", "Ameixa"];
+// let frutas = ["Banana", "Maçã", "Uva", "Pêra", "Melancia", "Morango", "Goiaba", "Ameixa"];
 
-console.log(frutas);
+// console.log(frutas);
 
-/* a propriedade length retorna o tamanho do array */
-console.log(`Tamanho do array: ${frutas.length}`);
+// /* a propriedade length retorna o tamanho do array */
+// console.log(`Tamanho do array: ${frutas.length}`);
 
-/* o método push adiciona um elemento no final do array */
-frutas.push("Laranja");
-console.table(frutas);
+// /* o método push adiciona um elemento no final do array */
+// frutas.push("Laranja");
+// console.table(frutas);
 
-/* o método unshift adiciona um elemento no começo do array */
-frutas.unshift("Abacaxi");
-console.table(frutas);
+// /* o método unshift adiciona um elemento no começo do array */
+// frutas.unshift("Abacaxi");
+// console.table(frutas);
 
-/* o método pop remove o último elemento do array */
-frutas.pop();
-console.table(frutas);
+// /* o método pop remove o último elemento do array */
+// frutas.pop();
+// console.table(frutas);
 
-/* o método shift remove o primeiro elemento do array */
-frutas.shift();
-console.table(frutas);
+// /* o método shift remove o primeiro elemento do array */
+// frutas.shift();
+// console.table(frutas);
 
-/* o método indexOf retorna o índice do primeiro elemento encontrado */
-let indice = frutas.indexOf("Morango");
-console.log(indice);
-console.log(frutas[indice])
+// /* o método indexOf retorna o índice do primeiro elemento encontrado */
+// let indice = frutas.indexOf("Morango");
+// console.log(indice);
+// console.log(frutas[indice])
 
-/* a forma manual de fazer isso sem utilizar indexOf é: */
-console.log("FRUTA na posição 6: " + frutas[5]);
+// /* a forma manual de fazer isso sem utilizar indexOf é: */
+// console.log("FRUTA na posição 6: " + frutas[5]);
 
-/* o método splice remove e adiciona elementos no array */
-/* observação: o primeiro parâmetro é o índice que queremos remover e o segundo parâmetro é quantas vezes queremos remover o índice informado no primeiro parâmetro */
-frutas.splice(3, 1);
-console.table(frutas);
+// /* o método splice remove e adiciona elementos no array */
+// /* observação: o primeiro parâmetro é o índice que queremos remover e o segundo parâmetro é quantas vezes queremos remover o índice informado no primeiro parâmetro */
+// frutas.splice(3, 1);
+// console.table(frutas);
 
-/* o método forEach executa uma função para cada elemento do array */
-/* observação: o método forEach sempre recebe uma função como parâmetro. No caso abaixo, estamos usando uma arrow function */
-frutas.forEach( (fruta) => {
-    console.log(fruta);
-});
+// /* o método forEach executa uma função para cada elemento do array */
+// /* observação: o método forEach sempre recebe uma função como parâmetro. No caso abaixo, estamos usando uma arrow function */
+// frutas.forEach( (fruta) => {
+//     console.log(fruta);
+// });
+
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+
+/* concatenando arrays com o operador spread*/
+/* observação: o operador spread é identificado por "..." */
+let nr3 = [...nr1, ...nr2];
+console.log(`NOVO ARRAY: ${nr3}`);
+
+/* percorrendo arrays com forEach*/
+nr3.forEach( (nr) => {
+    console.log(`ITEM DO NOVO ARRAY COM SPREAD: ${nr}`);
+})
 
